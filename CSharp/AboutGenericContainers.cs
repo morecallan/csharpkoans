a2ceb52db06fe9a35194ca5b44796c6259416caf
Callan Morrison <callan.l.morrison@gmail.com>
2016-07-25 20:30:35 -0500

    koan10

--- CSharp/AboutGenericContainers.cs
+++ CSharp/AboutGenericContainers.cs
@@ -120,7 +120,7 @@ namespace DotNetKoans.CSharp
 		{
 			//Just as with array, you can use the subscript notation to access any element in a list.
 			List<int> list = new List<int> { 5, 6, 7 };
-			Assert.Equal(FILL_ME_IN, list[2]);
+			Assert.Equal(7, list[2]);
 		}
 		[Koan(11)]
 		public void BeyondTheLimits()
