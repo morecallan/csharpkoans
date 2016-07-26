4ed032ab1a7884c064d313ac4ea92340618455a8
Callan Morrison <callan.l.morrison@gmail.com>
2016-07-25 21:02:30 -0500

    koan16

--- CSharp/AboutGenericContainers.cs
+++ CSharp/AboutGenericContainers.cs
@@ -166,15 +166,15 @@ namespace DotNetKoans.CSharp
 		public void GenericStackPushPop()
 		{
 			var stack = new Stack<int>();
-			Assert.Equal(FILL_ME_IN, stack.Count);
+			Assert.Equal(0, stack.Count);
 
 			stack.Push(42);
-			Assert.Equal(FILL_ME_IN, stack.Count);
+			Assert.Equal(1, stack.Count);
 
 			int x = stack.Pop();
-			Assert.Equal(FILL_ME_IN, x);
+			Assert.Equal(42, x);
 
-			Assert.Equal(FILL_ME_IN, stack.Count);
+			Assert.Equal(0, stack.Count);
 		}
 		[Koan(17)]
 		public void StackOrder()
