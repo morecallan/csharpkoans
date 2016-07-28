2c6d5d229c8e9faec42267f0b51a13b407368ac9
Callan Morrison <callan.l.morrison@gmail.com>
2016-07-27 20:30:28 -0500

    koan5

--- CSharp/AboutStrings.cs
+++ CSharp/AboutStrings.cs
@@ -49,7 +49,7 @@ namespace DotNetKoans.CSharp
         {
             var strA = @"Verbatim Strings can handle both ' and "" characters (when escaped)";
             var strB = "Verbatim Strings can handle both ' and \" characters (when escaped)";
-            Assert.Equal(FILL_ME_IN, strA.Equals(strB));
+            Assert.Equal(true, strA.Equals(strB));
         }
 
         [Koan(6)]
